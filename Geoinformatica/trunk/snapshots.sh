18c45387b85f43b08b9bb41a49d93624bdd22169
ajolma <ajolma@d058f3b2-7357-0410-b50b-e66f4b7e35bb>
2008-10-08 20:24:06 +0000

    libral uses now gdal-config
    git-svn-id: https://svn.osgeo.org/geoinformatica@52 d058f3b2-7357-0410-b50b-e66f4b7e35bb

--- Geoinformatica/trunk/snapshots.sh
+++ Geoinformatica/trunk/snapshots.sh
@@ -1,101 +1,109 @@
 #!/bin/sh
 
-export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/lib/pkgconfig
-export PATH=$PATH:/usr/local/bin
-
+PUBLISHER=jolma@map.hut.fi
 MAP_WWW=/var/www/html
+HOME=/home/ajolma/dev
+PREFIX=/home/ajolma/usr
 
-cd /home/ajolma
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/lib/pkgconfig:$PREFIX/lib/pkgconfig
+export PATH=$PATH:/usr/local/bin
+export PERL5LIB=$PREFIX/lib/perl5:$PREFIX/lib/perl5/site_perl/5.8.8:$PREFIX/lib/perl5/i386-linux-thread-multi
+export LD_LIBRARY_PATH=$HOME/gdal/.libs:$HOME/geoinformatica/libral/trunk/.libs
+export PROJSO=/usr/local/lib/libproj.so
+
+
+cd $HOME
+
 rm -rf gdal
 svn checkout https://svn.osgeo.org/gdal/trunk/gdal gdal
+
 cd gdal
-./configure --with-pic --with-libtiff=internal
-make 
+./configure --prefix=$PREFIX --with-pic --with-libtiff=internal
+make
 make install
+
 cd swig/perl
 rm *wrap*
 make generate
+perl Makefile.PL PREFIX=$PREFIX
 make build
 make test
 make install
 doxygen
 cd html
-scp * jolma@map.hut.fi:$MAP_WWW/doc/Geo-GDAL/html
-cd ..
+scp * $PUBLISHER:$MAP_WWW/doc/Geo-GDAL/html
 
-cd /home/ajolma/hoslab
-rm -rf svngeoinformatica
-svn co svn+ssh://ajolma@hoslab.cs.helsinki.fi/svngeoinformatica
+cd $HOME
+rm -rf geoinformatica
+svn co https://svn.osgeo.org/geoinformatica/
 
-cd svngeoinformatica/libral/trunk
+cd geoinformatica/libral/trunk
 sh autogen.sh
-./configure
+./configure --prefix=$PREFIX --with-gdal=$PREFIX/bin/gdal-config
 make
 make install
 make dist
-scp *.tar.gz jolma@map.hut.fi:$MAP_WWW/files/Geoinformatica/snapshots
+scp *.tar.gz $PUBLISHER:$MAP_WWW/files/Geoinformatica/snapshots
 cp -f *.tar.gz ../../..
 rm *.tar.gz
 doxygen
 cd html
-scp * jolma@map.hut.fi:$MAP_WWW/doc/libral/html
+scp * $PUBLISHER:$MAP_WWW/doc/libral/html
 cd ..
 
+# perl modules
+
 cd ../../IPC-Gnuplot/trunk/
-perl Makefile.PL
+perl Makefile.PL PREFIX=$PREFIX
 make
 make test
 make install
 make dist
-scp *.tar.gz jolma@map.hut.fi:$MAP_WWW/files/Geoinformatica/snapshots
+scp *.tar.gz $PUBLISHER:$MAP_WWW/files/Geoinformatica/snapshots
 cp -f *.tar.gz ../../..
 rm *.tar.gz
 
 cd ../../Geo-OGC-Geometry/trunk/
-perl Makefile.PL
+perl Makefile.PL PREFIX=$PREFIX
 make
 make test
 make install
 make dist
-scp *.tar.gz jolma@map.hut.fi:$MAP_WWW/files/Geoinformatica/snapshots
+scp *.tar.gz $PUBLISHER:$MAP_WWW/files/Geoinformatica/snapshots
 cp -f *.tar.gz ../../..
 rm *.tar.gz
 
 cd ../../Geo-Raster/trunk/
-perl Makefile.PL
+perl Makefile.PL PREFIX=$PREFIX
 make
 make test
 make install
 make dist
-scp *.tar.gz jolma@map.hut.fi:$MAP_WWW/files/Geoinformatica/snapshots
+scp *.tar.gz $PUBLISHER:$MAP_WWW/files/Geoinformatica/snapshots
 cp -f *.tar.gz ../../..
 rm *.tar.gz
 doxygen
 cd html
-scp * jolma@map.hut.fi:$MAP_WWW/doc/Geoinformatica/html
+scp * $PUBLISHER:$MAP_WWW/doc/Geoinformatica/html
 cd ..
 
 cd ../../Geo-Vector/trunk/
-perl Makefile.PL
+perl Makefile.PL PREFIX=$PREFIX
 make
 make test
 make install
 make dist
-scp *.tar.gz jolma@map.hut.fi:$MAP_WWW/files/Geoinformatica/snapshots
+scp *.tar.gz $PUBLISHER:$MAP_WWW/files/Geoinformatica/snapshots
 cp -f *.tar.gz ../../..
 rm *.tar.gz
 
 cd ../../Gtk2-Ex-Geo/trunk/
-perl Makefile.PL
+perl Makefile.PL PREFIX=$PREFIX
 make
 make test
 make install
 make dist
-scp *.tar.gz jolma@map.hut.fi:$MAP_WWW/files/Geoinformatica/snapshots
+scp *.tar.gz $PUBLISHER:$MAP_WWW/files/Geoinformatica/snapshots
 cp -f *.tar.gz ../../..
 rm *.tar.gz
 
-cd ../../..
-
-cp -f svngeoinformatica/Geoinformatica/trunk/gui.pl /usr/local/bin/gis.pl
-chmod ugo+x /usr/local/bin/gis.pl
