2a8736b34337a0bd85f74a99ce608b1495c8daea
ajolma <ajolma@d058f3b2-7357-0410-b50b-e66f4b7e35bb>
2008-11-05 13:52:01 +0000

    put geoinformatica first in path
    git-svn-id: https://svn.osgeo.org/geoinformatica@140 d058f3b2-7357-0410-b50b-e66f4b7e35bb

--- Geoinformatica/trunk/G-shell.bat
+++ Geoinformatica/trunk/G-shell.bat
@@ -1,4 +1,4 @@
 set GDAL_DATA=c:\program files\geoinformatica\share\gdal
 set PROJSO=c:\program files\geoinformatica\bin\libproj.dll
-set PATH=%PATH%;"c:\program files\geoinformatica\bin"
+set PATH="c:\program files\geoinformatica\bin";%PATH%
 cmd
