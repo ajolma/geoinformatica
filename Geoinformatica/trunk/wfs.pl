ff42817302d7045e65ecab4bd1f678a9430d4a4a
ajolma <ajolma@d058f3b2-7357-0410-b50b-e66f4b7e35bb>
2011-11-17 14:39:08 +0000

    initial wfs server and an example configuration file
    git-svn-id: https://svn.osgeo.org/geoinformatica@444 d058f3b2-7357-0410-b50b-e66f4b7e35bb

---
+++ Geoinformatica/trunk/wfs.pl
@@ -0,0 +1,226 @@
+#!/usr/bin/perl
+
+use strict;
+use Carp;
+use Encode;
+use DBI;
+use CGI;
+use Geo::Proj4;
+use Gtk2::Ex::Geo;
+use Geo::Raster;
+use Geo::Vector;
+use JSON;
+
+my $config;
+{
+    open(my $fh, '<', "wfs.conf") or die $!;
+    my @json = <$fh>;
+    close $fh;
+    $config = decode_json "@json";
+}
+my $q = CGI->new;
+my %names = ();
+
+eval {
+    page();
+};
+if ($@) {
+    #print STDERR "WFS error: $@\n";
+    print $q->header($config->{MIME});
+    print('<?xml version="1.0" encoding="UTF-8"?>',"\n");
+    my($error) = ($@ =~ /(.*?)\.\#/);
+    $error =~ s/ $//;
+    $error = { code => $error } if $error eq 'LayerNotDefined';
+    $error = 'Unspecified error: '.$@ unless $error;
+    xml_element('ServiceExceptionReport',['ServiceException', $error]);
+}
+
+sub page {
+    for ($q->param) {
+	croak "Parameter ".uc($_)." given more than once.#" if exists $names{uc($_)};
+	$names{uc($_)} = $_;
+    }
+    $q->{resource} = $config->{resource};
+    my $request = $q->param($names{REQUEST});
+    my $version = $q->param($names{WMTVER});
+    $version = $q->param($names{VERSION}) if $q->param($names{VERSION});
+    $version = '1.1.0' unless $version;
+    #croak "Not a supported WFS version.#" unless $version eq $config->{version};
+    my $service = $q->param($names{SERVICE});
+    $service = 'WFS'unless $service;
+    if ($request eq 'GetCapabilities' or $request eq 'capabilities') {
+	GetCapabilities($version);
+    } elsif ($request eq 'DescribeFeatureType') {
+	DescribeFeatureType($version, $q->param($names{TYPENAME}));
+    } else {
+	print 
+	    $q->header(), 
+	    $q->start_html, 
+	    $q->a({-href=>$q->{resource}.'REQUEST=GetCapabilities'}, "GetCapabilities"), 
+	    $q->end_html;
+    }
+}
+
+sub GetCapabilities {
+    my($version) = @_;
+    print($q->header( -type => $config->{MIME} ));
+    print('<?xml version="1.0" encoding="UTF-8"?>',"\n");
+    xml_element('wfs:WFS_Capabilities', 
+		{ version => $version,
+		  'xmlns:gml' => "http://www.opengis.net/gml",
+		  'xmlns:wfs' => "http://www.opengis.net/wfs",
+		  'xmlns:ows' => "http://www.opengis.net/ows",
+		  'xmlns:xlink' => "http://www.w3.org/1999/xlink",
+		  'xmlns:xsi' => "http://www.w3.org/2001/XMLSchema-instance",
+		  'xmlns:ogc' => "http://www.opengis.net/ogc",
+		  'xmlns' => "http://www.opengis.net/wfs",
+		  'xsi:schemaLocation' => "http://www.opengis.net/wfs http://schemas.opengis.net/wfs/1.1.0/wfs.xsd" }, 
+		'<>');
+    ServiceIdentification($version);
+    ServiceProvider($version);
+    OperationsMetadata($version);
+    FeatureTypeList($version);
+    Filter_Capabilities($version);
+    xml_element('/wfs:WFS_Capabilities', '<>');
+}
+
+sub ServiceIdentification {
+    my($version) = @_;
+    xml_element('ows:ServiceIdentification', '<>');
+    xml_element('ows:Title', 'WFS Server');
+    xml_element('ows:Abstract');
+    xml_element('ows:ServiceType', {codeSpace=>"OGC"}, 'OGC WFS');
+    xml_element('ows:ServiceTypeVersion', $version);
+    xml_element('ows:Fees');
+    xml_element('ows:AccessConstraints');
+    xml_element('/ows:ServiceIdentification', '<>');
+}
+
+sub ServiceProvider {
+    my($version) = @_;
+    xml_element('ows:ServiceProvider', '<>');
+    xml_element('ows:ProviderName');
+    xml_element('ows:ProviderSite', {'xlink:type'=>"simple", 'xlink:href'=>""});
+    xml_element('ows:ServiceContact');
+    xml_element('/ows:ServiceProvider', '<>');
+}
+
+sub OperationsMetadata  {
+    my($version) = @_;
+    xml_element('ows:OperationsMetadata', '<>');
+    Operation($version, 'GetCapabilities', 
+	      [{service => ['WFS']}, {AcceptVersions => ['1.1.0','1.0.0']}, {AcceptFormats => ['text/xml']}]);
+    Operation($version, 'DescribeFeatureType', 
+	      [{outputFormat => ['XMLSCHEMA','text/xml; subtype=gml/2.1.2','text/xml; subtype=gml/3.1.1']}]);
+    Operation($version, 'GetFeature',
+	      [{resultType => ['results']}, {outputFormat => ['text/xml; subtype=gml/3.1.1']}]);
+    xml_element('/ows:OperationsMetadata', '<>');
+}
+
+sub Operation {
+    my($version, $name, $parameters) = @_;
+    my @parameters;
+    for my $p (@$parameters) {
+	for my $n (keys %$p) {
+	    my @values;
+	    for my $v (@{$p->{$n}}) {
+		push @values, ['ows:Value', $v];
+	    }
+	    push @parameters, ['ows:Parameter', {name=>$n}, \@values];
+	}
+    }
+    xml_element('ows:Operation', 
+		{name => $name}, 
+		[['ows:DCP', 
+		  ['ows:HTTP', [['ows:Get', {'xlink:type'=>'simple', 'xlink:href'=>$config->{resource}}],
+				['ows:Post', {'xlink:type'=>'simple', 'xlink:href'=>$config->{resource}}]]
+		  ]],@parameters]);
+}
+
+sub FeatureTypeList  {
+    my($version) = @_;
+    xml_element('FeatureTypeList', '<>');
+    xml_element('Operations', ['Operation', 'Query']);
+    for my $type (@{$config->{FeatureTypeList}}) {
+	xml_element('FeatureType', [
+			['Name', $type->{Name}],
+			['Title', $type->{Title}],
+			['DefaultSRS', $type->{DefaultSRS}],
+			['OutputFormats', ['Format', 'text/xml; subtype=gml/3.1.1']],
+			['ows:WGS84BoundingBox', {dimensions=>2}, 
+			 [['ows:LowerCorner',$type->{LowerCorner}],
+			  ['ows:UpperCorner',$type->{UpperCorner}]]]
+		    ]);
+    }
+    xml_element('/FeatureTypeList', '<>');
+}
+
+sub Filter_Capabilities  {
+    my($version) = @_;
+    xml_element('ogc:Filter_Capabilities', '<>');
+    my @operands = ();
+    for my $o ('Point','LineString','Polygon','Envelope') {
+	push @operands, ['ogc:GeometryOperand', 'gml:'.$o];
+    }
+    my @operators = ();
+    for my $o ('Equals','Disjoint','Touches','Within','Overlaps','Crosses','Intersects','Contains','DWithin','Beyond','BBOX') {
+	push @operators, ['ogc:SpatialOperator', {name=>$o}];
+    }
+    xml_element('ogc:Spatial_Capabilities', 
+		[['ogc:GeometryOperands', \@operands],
+		 ['ogc:SpatialOperators', \@operators]]);
+    @operators = ();
+    for my $o ('LessThan','GreaterThan','LessThanEqualTo','GreaterThanEqualTo','EqualTo','NotEqualTo','Like','Between') {
+	push @operators, ['ogc:ComparisonOperator', $o];
+    }
+    xml_element('ogc:Scalar_Capabilities', 
+		[['ogc:LogicalOperators'],
+		 ['ogc:ComparisonOperators', \@operators]]);
+    xml_element('ogc:Id_Capabilities', ['ogc:FID']);
+    xml_element('/ogc:Filter_Capabilities', '<>');
+}
+
+sub xml_elements {
+    for my $e (@_) {
+	if (ref($e) eq 'ARRAY') {
+	    xml_element(@$e);
+	} else {
+	    xml_element($e->{element}, $e->{attributes}, $e->{content});
+	}
+    }
+}
+
+sub xml_element {
+    my $element = shift;
+    my $attributes;
+    my $content;
+    for my $x (@_) {
+	$attributes = $x, next if ref($x) eq 'HASH';
+	$content = $x;
+    }
+    print("<$element");
+    if ($attributes) {
+	for my $a (keys %$attributes) {
+	    print(" $a=\"$attributes->{$a}\"");
+	}
+    }
+    unless ($content) {
+	print("/>\n");
+    } else {
+	if (ref $content) {
+	    print(">");
+	    if (ref $content->[0]) {
+		for my $e (@$content) {
+		    xml_element(@$e);
+		}
+	    } else {
+		xml_element(@$content);
+	    }
+	    print("</$element>\n");
+	} elsif ($content eq '<>') {
+	    print(">\n");
+	} elsif ($content) {
+	    print(">$content</$element>\n");	
+	}
+    }
+}
