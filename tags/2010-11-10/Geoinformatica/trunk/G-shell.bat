2878758d23954e28988bf70355a3b07b1d3948fd
Ari Jolma <ajolma@lenovo.(none)>
2012-02-14 23:36:50 +0200

    removes

--- tags/2010-11-10/Geoinformatica/trunk/G-shell.bat
+++
@@ -1,4 +0,0 @@
-set GDAL_DATA=c:\program files\geoinformatica\share\gdal
-set PROJSO=c:\program files\geoinformatica\bin\libproj.dll
-set PATH="c:\program files\geoinformatica\bin";%PATH%
-cmd
--- tags/2010-11-10/Geoinformatica/trunk/modules.pl
+++
@@ -1,8 +0,0 @@
-opendir(DIR, 'c:/dev/CPAN');
-@d = readdir (DIR);
-closedir DIR;
-for (sort @d) {
-    next unless /^[A-W]/;
-    next if /^Test/;
-    print "$_\n";
-}
