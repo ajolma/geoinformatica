7715484a2359f12f4f325c02df5249f0ed890f83
Ari Jolma <ari.jolma@gmail.com>
2013-02-03 07:13:03 -0800

    https://rt.cpan.org/Public/Bug/Display.html?id=82865

--- Gtk2-Ex-Geo/lib/Gtk2/Ex/Geo.pm
+++ Gtk2-Ex-Geo/lib/Gtk2/Ex/Geo.pm
@@ -106,7 +106,7 @@ BEGIN {
     our %EXPORT_TAGS = ( 'all' => [ qw( ) ] );
     our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
     our @EXPORT = qw( );
-    our $VERSION = '0.64';
+    our $VERSION = '0.65';
     XSLoader::load( 'Gtk2::Ex::Geo', $VERSION );
 }
 
