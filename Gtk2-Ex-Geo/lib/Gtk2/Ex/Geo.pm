db3853ec1d20d38bfc9e2fc7d8bf329c671d9fcf
Ari Jolma <ari.jolma@gmail.com>
2013-02-03 00:47:30 -0800

    Fixed a bug in configure: https://rt.cpan.org/Public/Bug/Display.html?id=82865

--- Gtk2-Ex-Geo/lib/Gtk2/Ex/Geo.pm
+++ Gtk2-Ex-Geo/lib/Gtk2/Ex/Geo.pm
@@ -106,7 +106,7 @@ BEGIN {
     our %EXPORT_TAGS = ( 'all' => [ qw( ) ] );
     our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
     our @EXPORT = qw( );
-    our $VERSION = '0.63';
+    our $VERSION = '0.64';
     XSLoader::load( 'Gtk2::Ex::Geo', $VERSION );
 }
 
